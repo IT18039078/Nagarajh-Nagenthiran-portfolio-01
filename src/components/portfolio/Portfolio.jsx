4105a361fc5b26d7ec4dfa82fc96a542df40cc05
IT18039078 <IT18039078@my.sliit.lk>
2022-07-28 11:52:14 +0530

    The testimonial section was successfully finished

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './portfolio.css'
-
+import IMG from '../../assets/portfolio1.png'
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section >
+    <section id='portfolio'>
+      <h5>My Recent Wokr</h5>
+      <h2>Portfolio</h2>
+
+      {/* IN THE REFERNCE 2.17.02 IS THE PLACE I SHOULD LOOT AT TO CHANGE THIS AS ARRAY WHEN
+      IT IS GETTING BIGGER  */}
+      <div className="container portfolio__container">
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+                <img src={IMG} alt="porfolio" />
+            </div>
+            <h3>This is portfolio item title</h3>
+            <div className="portfolio__item-cta">
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Github</a>
+            </div>
+          </article>
+      </div>    
+    </section >
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './testimonials.css'
+import AVAT1 from '../../assets/nageprofile.jpg'
 
+
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// ARRAY DATA FOR TESTIMONIAL
+const data =[
+  {
+    avatar: AVAT1,
+    name: 'Nagenthiran',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo vero nihil corporis rerum facilis fugiat dolorum assumenda modi vitae ratione nostrum, mollitia ducimus consequatur nam dolorem totam, aspernatur quis!"
+  },
+  {
+    avatar: AVAT1,
+    name: 'Pasapukal',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo vero nihil corporis rerum facilis fugiat dolorum assumenda modi vitae ratione nostrum, mollitia ducimus consequatur nam dolorem totam, aspernatur quis!"
+  },
+  {
+    avatar: AVAT1,
+    name: 'Nagenthiran',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo vero nihil corporis rerum facilis fugiat dolorum assumenda modi vitae ratione nostrum, mollitia ducimus consequatur nam dolorem totam, aspernatur quis!"
+  }
+]
 const Testimonials = () => {
   return (
-    <section id='testimonial'>Testimonials</section >
+    <section id='testimonial'>
+      <h5>Rreview from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+       // install Swiper modules
+      
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      autoplay={true}
+      activeDotColor={'white'}
+      >
+        {
+            data.map(({avatar, name, review}, index)=>{
+              return (
+                <SwiperSlide key={index} className="testimonial">
+                  <div className="client__avatar">
+                    <img src={avatar} alt="" />
+                  </div>
+                  <h5 className='clinet__name'>{name}</h5>
+                  <small className='client__review'>
+                    {review}
+                  </small>
+                </SwiperSlide>
+              )
+            })
+        }
+        </Swiper>
+    </section >
   )
 }
 
