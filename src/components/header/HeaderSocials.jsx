0a6f775a737ade6b076309c53b7b4eab7e8fe26d
IT18039078 <IT18039078@my.sliit.lk>
2022-08-21 19:40:32 +0530

    the latest changes were addded

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
+        <a href="https://github.com/NageWC1" target="_blank"><BsGithub/></a>
     </div>
   )
 }
